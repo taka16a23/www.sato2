cf8a334daa5220e98943d29c2b5efe529f8676e7
a23 <takahiroatsumi0517@gmail.com>
2024-05-12 00:25:30 +0900

    Add model

---
+++ backend/security/models/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""Name: __init__.py
+
+
+"""
+from security.models.security_knowledge_model import SecurityKnowledgeModel
+
+
+__all__ = [
+    'SecurityKnowledgeModel',
+]
+
+
+
+# For Emacs
+# Local Variables:
+# coding: utf-8
+# End:
+# __init__.py ends here
