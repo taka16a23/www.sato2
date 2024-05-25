2bce0e28c78d8be8d288494aba534d9c38016ae7
a23 <takahiroatsumi0517@gmail.com>
2024-05-25 12:12:30 +0900

    class => className

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -50,7 +50,7 @@ class HomeComponent extends Component {
               <h2 className="main-title">
                 <a className="link" href="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;mode=MONTH&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=satotanakami%40gmail.com&amp;color=%23125A12&amp;ctz=Asia%2FTokyo" alt="" title="カレンダー" target="_blank">
                   <span className="title">当面の予定</span>
-                  <i class="mdi mdi-open-in-new"></i>
+                  <i className="mdi mdi-open-in-new"></i>
                 </a>
               </h2>
               <div className="main-body calendar">
