8f4b2084baeea84e2deacccf1f4937bf59a674de
a23 <takahiroatsumi0517@gmail.com>
2025-01-24 15:55:14 +0900

    Implement

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,94 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom'
 
+import { ServiceFactory } from 'services';
 import AliasRoutes from "routes/AliasRoutes";
+import {
+  setActivityYear,
+  setActivityModels,
+} from 'redux/activity/Action';
+import {
+  setBoardYear,
+  setBoardModels,
+} from "redux/board/Action";
 
 
 class Footer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.newsYears = [];
+    this.activityYears = [];
+    this.boardYears = [];
+    this.state = {
+      newsYearsLength: this.newsYears.length,
+      activityYearsLength: this.activityYears.length,
+      boardYearsLength: this.boardYears.length,
+    }
+  }
+
+  componentDidMount() {
+    var service = ServiceFactory.createNewsService();
+    service.listYears().then(years => {
+      this.newsYears = years;
+      this.setState({
+        newsYearsLength: this.newsYears.length,
+      });
+    }).catch(err => {
+      alert(err);
+    });
+    var service = ServiceFactory.createActivitiesService();
+    service.listYears().then(years => {
+      this.activityYears = years;
+      this.setState({
+        activityYearsLength: this.activityYears.length,
+      });
+    }).catch(err => {
+      alert(err);
+    });
+    var service = ServiceFactory.createBoardsService();
+    service.listYears().then(years => {
+      this.boardYears = years;
+      this.setState({
+        boardYearsLength: this.boardYears.length,
+      });
+    }).catch(err => {
+      alert(err);
+    });
+
+  }
+
+  handleNewsOnClick(year) {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  handleActivityOnClick(year) {
+    this.props.setActivityYear(year);
+    let service = ServiceFactory.createActivitiesService();
+    service.listActivities(year).then(arrModels => {
+      this.props.setActivityModels(arrModels);
+    }).catch(err => {
+      alert(err);
+    });
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  handleBoardOnClick(year) {
+    this.props.setBoardYear(year);
+    let service = ServiceFactory.createBoardsService();
+    service.listBoards(year).then(arrModels => {
+      this.props.setBoardModels(arrModels);
+    }).catch(err => {
+      alert(err);
+    });
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  handleOnClick(ev) {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
 
   render() {
     return (
@@ -15,105 +99,121 @@ class Footer extends Component {
               <div className="sitemap-area">
                 <div className="sitemap sitemap-news">
                   <h3 className="sitemap-title">
-                    <a href="/news/">おしらせ</a>
+                    <NavLink to={AliasRoutes.News}>おしらせ</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
+                    {this.newsYears.map((year) =>
+                      <li key={year} className="sitemap-item">
+                        <NavLink to={AliasRoutes.NewsYear + "/" + year} title={year + "年のお知らせを見る"} onClick={() => this.handleNewsOnClick(year)}>
+                          <i className="mdi mdi-menu-right"></i>
+                          {year + '年'}
+                        </NavLink>
+                      </li>
+                    )}
                   </ul>
                 </div>
                 <div className="sitemap sitemap-activity">
                   <h3 className="sitemap-title">
-                    <a href="/activity/">ご報告</a>
+                    <NavLink to={AliasRoutes.Activities}>ご報告</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
+                    {this.activityYears.map((year) =>
+                      <li key={year} className="sitemap-item">
+                        <NavLink to={AliasRoutes.ActivitiesYear + "/" + year} title={year + "年の活動を見る"} onClick={() => this.handleActivityOnClick(year)}>
+                          <i className="mdi mdi-menu-right"></i>
+                          {year + '年'}
+                        </NavLink>
+                      </li>
+                    )}
                   </ul>
                 </div>
                 <div className="sitemap sitemap-board">
                   <h3 className="sitemap-title">
-                    <a href="/board/">回覧板</a>
+                    <NavLink to={AliasRoutes.Board}>回覧板</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
-                    <li className="sitemap-item"><a href="/news/?year=2000"><i className="mdi mdi-menu-right"></i>2000年</a></li>
+                    {this.activityYears.map((year) =>
+                      <li key={year} className="sitemap-item">
+                        <NavLink to={AliasRoutes.BoardsYear + "/" + year} title={year + "年の回覧を見る"} onClick={() => this.handleBoardOnClick(year)}>
+                          <i className="mdi mdi-menu-right"></i>
+                          {year + '年'}
+                        </NavLink>
+                      </li>
+                    )}
                   </ul>
                 </div>
                 <div className="sitemap sitemap-security">
                   <h3 className="sitemap-title">
-                    <a href="/security/">防犯・防災</a>
+                    <NavLink to={AliasRoutes.Security} onClick={() => this.handleOnClick()}>防犯・防災</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/security/secportal/"><i className="mdi mdi-menu-right"></i>統合情報</a></li>
-                    <li className="sitemap-item"><a href="/security/secportal/#evacuationMapArea"><i className="mdi mdi-menu-right"></i>避難地図</a></li>
-                    <li className="sitemap-item"><a href="/security/knowledge/"><i className="mdi mdi-menu-right"></i>防災予備知識</a></li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.SecurityPortal} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        統合情報
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.SecurityKnowledge} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        防災予備知識
+                      </NavLink>
+                    </li>
                   </ul>
                 </div>
                 <div className="sitemap sitemap-formats">
                   <h3 className="sitemap-title">
-                    <a href="/formats/">各種申請</a>
+                    <NavLink to={AliasRoutes.Format} onClick={() => this.handleOnClick()}>各種申請</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/formats/#formatSato"><i className="mdi mdi-menu-right"></i>里自治会関係書式</a></li>
-                    <li className="sitemap-item"><a href="/formats/#formatETC"><i className="mdi mdi-menu-right"></i>自治体等関係書式</a></li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.Formats + "/#sato"} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        里自治会関係書式
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.Formats + "/#other"} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        自治体等関係書式
+                      </NavLink>
+                    </li>
                   </ul>
                 </div>
                 <div className="sitemap sitemap-about">
                   <h3 className="sitemap-title">
-                    <a href="/about/">自治会案内</a>
+                    <NavLink to={AliasRoutes.About} onClick={() => this.handleOnClick()}>自治会案内</NavLink>
                   </h3>
                   <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/about/rule/"><i className="mdi mdi-menu-right"></i>会則</a></li>
-                    <li className="sitemap-item"><a href="/about/hall/"><i className="mdi mdi-menu-right"></i>公民館</a></li>
-                    <li className="sitemap-item"><a href="/about/group/"><i className="mdi mdi-menu-right"></i>ブロックと組</a></li>
-                    <li className="sitemap-item"><a href="/about/solve/#faq"><i className="mdi mdi-menu-right"></i>よくある質問</a></li>
-                    <li className="sitemap-item"><a href="/about/solve/#contact"><i className="mdi mdi-menu-right"></i>情報提供・お問い合わせ</a></li>
-                  </ul>
-                </div>
-                <div className="sitemap sitemap-etc">
-                  <h3 className="sitemap-title">その他</h3>
-                  <ul className="sitemap-list">
-                    <li className="sitemap-item"><a href="/news/feed/"><i className="mdi mdi-menu-right"></i>RSS</a></li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.AboutRule} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        会則
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.AboutHall} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>公民館
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.AboutGroup} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        ブロックと組
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.AboutQuery} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        よくある質問
+                      </NavLink>
+                    </li>
+                    <li className="sitemap-item">
+                      <NavLink to={AliasRoutes.AboutQuery} onClick={() => this.handleOnClick()}>
+                        <i className="mdi mdi-menu-right"></i>
+                        情報提供・お問い合わせ
+                      </NavLink>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -132,7 +232,7 @@ class Footer extends Component {
               </div>
             </div>
             <div className="copyright">
-              <small>Copyright&copy; 2016 taka16a23 All Rights Reserved.</small>
+              <small>Copyright&copy; 2016 <a href="https://taka16a23.com" ref="noreferrer" target="_blank">taka16a23</a> All Rights Reserved.</small>
             </div>
           </div>
         </footer>
@@ -146,4 +246,35 @@ class Footer extends Component {
   };
 }
 
-export default Footer;
+const mapStateToProps = (state) => {
+  return {
+    activity: {
+      year: state.activity.year,
+      models: state.activity.models,
+    },
+    boards: {
+      year: state.board.year,
+      models: state.board.models,
+    }
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setActivityYear: function(year) {
+      dispatch(setActivityYear(year));
+    },
+    setActivityModels: function(year) {
+      dispatch(setActivityModels(year));
+    },
+    setBoardYear: function(year) {
+      dispatch(setBoardYear(year));
+    },
+    setBoardModels: function(year) {
+      dispatch(setBoardModels(year));
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
