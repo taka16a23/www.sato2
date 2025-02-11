b4ac7a64c4bf445d2bcae76d2d22ef00781196ad
a23 <takahiroatsumi0517@gmail.com>
2025-02-11 18:59:40 +0900

    Add new tab link

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -70,7 +70,7 @@ class HomeComponent extends Component {
             </h2>
             <div className="side-body daily-today">
               <div className="daily-calendar">
-                <a href="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;mode=MONTH&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=satotanakami%40gmail.com&amp;color=%23125A12&amp;ctz=Asia%2FTokyo" alt="" title="今日">
+                <a href="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;mode=MONTH&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=satotanakami%40gmail.com&amp;color=%23125A12&amp;ctz=Asia%2FTokyo" alt="" target="_blank" title="今日">
                   <div className="daily-date daily-year daily-month">
                     <span>{this.state.today.getFullYear()}年{this.state.today.getMonth() + 1}月</span>
                   </div>
