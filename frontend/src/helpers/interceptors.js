3a2b91da96f830b3a4aeca64826e3f5a6a2b734c
a23 <takahiroatsumi0517@gmail.com>
2025-03-06 00:55:42 +0900

    Change comment

--- frontend/src/helpers/interceptors.js
+++ frontend/src/helpers/interceptors.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default function axiosSetUp() {
-  // point to your API endpoint
+  // API endpoint
   axios.defaults.baseURL = "<" + process.env.REACT_APP_API_URL + "/api/>";
 }
