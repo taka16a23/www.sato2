e2f04ef9fca3f113f6e32e5b1e2e0b0fcb049e08
Takahiro Atsumi <takahiroatsumi0517@gmail.com>
2025-09-28 08:28:12 +0900

    Update

--- frontend/src/helpers/withRouter.js
+++ frontend/src/helpers/withRouter.js
@@ -1,21 +1,8 @@
-import {
-  useLocation,
-  useNavigate,
-  useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-export function withRouter(Component) {
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    return (
-      <Component
-        {...props}
-        router={{ location, navigate, params }}
-      />
-    );
+export default function withRouter( Child ) {
+  return ( props ) => {
+    const params = useParams();
+    return <Child { ...props } params ={ params } />;
   }
-
-  return ComponentWithRouterProp;
 }
